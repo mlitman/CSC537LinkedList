d0af5185e6f5be5ae13f28c28133aff2ba3f19c3
Mike Litman <mike@awesomefat.com>
2015-10-28 22:06:07 -0500

    initial commit

---
+++ Driver.java
@@ -0,0 +1,14 @@
+
+public class Driver 
+{
+	public static void main(String[] args) throws Exception
+	{
+		LinkedList ll = new LinkedList();
+		ll.addFront(5);
+		ll.addFront(7);
+		ll.addEnd(3);
+		ll.removeFront();
+		ll.display();
+		
+	}
+}
